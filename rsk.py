0e6578ecfe4b4b6502be56054c8c8e26931fff1a
Rujuta Sunil Kherdekar <rujuta.kherdekar_ext@mediakind.com>
2026-03-10 22:06:13 +0530

    edited the code

--- rsk.py
+++ rsk.py
@@ -7,8 +7,8 @@ sum = int(num1) + int(num2)
 
 # Displaying the result
 print(f"The total is: {sum}") Asking for user input
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
 
 # Adding the numbers (int converts text to integers)
 sum = int(num1) + int(num2)
