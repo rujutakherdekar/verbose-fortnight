74650bfd9177831bf2500eb8046a59dfd1af8301
Rujuta Sunil Kherdekar <rujuta.kherdekar_ext@mediakind.com>
2026-03-10 21:48:39 +0530

    created python code

---
+++ rsk.py
@@ -0,0 +1,17 @@
+## Asking for user input
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Adding the numbers (int converts text to integers)
+sum = int(num1) + int(num2)
+
+# Displaying the result
+print(f"The total is: {sum}") Asking for user input
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Adding the numbers (int converts text to integers)
+sum = int(num1) + int(num2)
+
+# Displaying the result
+print(f"The total is: {sum}")
